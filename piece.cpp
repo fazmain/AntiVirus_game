e6a3e872aa23cc6cf1883b795779e14f579cbf53
fazmain <faiaz.azmain99@gmail.com>
2023-10-23 12:32:45 -0400

    "Intermediate changes"

--- piece.cpp
+++ piece.cpp
@@ -2,89 +2,112 @@
 // type is a 0-9 that indicated the type/shape of piece 
 // initialLocation is two ints referencing the indicy of the starting point of the piece in the 2d array
 // orientation is a 0-3 that indicates the orientation of the piece
+#include <exception>
+#include <string.h>
+using namespace std;
 
+class piece 
+{
 
-//location is SETS of coordinates that reference the location of all of the parts of the piece in the 2darray(board)
-int location[3][2];
-
-
-
-
-piece(int type, array initialLocation, int orientation){
-    //update location to include all spots the piece is on the board based on orrientation and initial location 
+    //location is SETS of coordinates that reference the location of all of the parts of the piece in the 2darray(board)
+    public:
+        int location[3][2];
+        board = board;
+    private:
+        int type;
+        int initialLocation[2];
+        int orientation;
+        
+    // constructor for piece
+    piece(int type, int initialLocation[2], int orientation){
+        //update location to include all spots the piece is on the board based on orrientation and initial location 
     /*
-- W: Blocker, a piece that takes up a single space and can not be moved 1.2.10
-- 0: Red Virus orrientation: 0=down 1=right 2=up 3=left
-- 1: Blue Virus orrientation: 0=down 1=right 2=up 3=left
-- 2: Orange Virus 1.2.3
-- 3: Pink Virus 1.2.4
-- 4: Dark Green Virus 1.2.5
-- 5: Blue Virus 1.2.6
-- 6: Purple Virus 1.2.7
-- 7: Light Green Virus 1.2.8
-- 8: Yellow Virus 1.2.9*/
-    location[0][0] = initialLocation[0];
-    location[0][1] = initialLocation[1];
-    switch (type)
-    {
-        case 0 || 1:
-            switch (orientation)
-            {
-            case 0:
-                location[1][0] = initialLocation[0] + 1;
-                location[1][1] = initialLocation[1];
-                break;
-            case 1:
-                location[1][0] = initialLocation[0];
-                location[1][1] = initialLocation[1] + 1;
-                break;
-            case 2:
-                location[1][0] = initialLocation[0] - 1;
-                location[1][1] = initialLocation[1];
-                break;
-            case 3:
-                location[1][0] = initialLocation[0];
-                location[1][1] = initialLocation[1] - 1;
-                break;
-            }
-            break;
-        break;
-        case 2:
-            break;
-
-    }
-    board.addPiece(type, location);
-}
-
-void movePiece(int piece, string direction, int steps){
-    
-    if(checkMove(piece, direction, steps)){
-        board.removePiece(piece);
-        //update location
-        switch (direction)
+    - W: Blocker, a piece that takes up a single space and can not be moved 1.2.10
+    - 0: Red Virus orrientation: 0=down 1=right 2=up 3=left
+    - 1: Blue Virus orrientation: 0=down 1=right 2=up 3=left
+    - 2: Orange Virus 1.2.3
+    - 3: Pink Virus 1.2.4
+    - 4: Dark Green Virus 1.2.5
+    - 5: Blue Virus 1.2.6
+    - 6: Purple Virus 1.2.7
+    - 7: Light Green Virus 1.2.8
+    - 8: Yellow Virus 1.2.9*/
+        location[0][0] = initialLocation[0];
+        location[0][1] = initialLocation[1];
+        switch (type)
         {
-        case "NE":
-            foreach location[][]
-                -1 to row
-            break;
-        case "NW":
-            foreach location[][]
-                -1 to column
-            break;
-        case "SE":
-            foreach location[][]
-                +1 to column
-            break;
-        case "SW":
-            foreach location[][]
-                +1 to row
-            break;
-        default:
+            case 0 || 1:
+                switch (orientation)
+                {
+                case 0:
+                    location[1][0] = initialLocation[0] + 1;
+                    location[1][1] = initialLocation[1];
+                break;
+                case 1:
+                    location[1][0] = initialLocation[0];
+                    location[1][1] = initialLocation[1] + 1;
+                break;
+                case 2:
+                    location[1][0] = initialLocation[0] - 1;
+                    location[1][1] = initialLocation[1];
+                break;
+                case 3:
+                    location[1][0] = initialLocation[0];
+                    location[1][1] = initialLocation[1] - 1;
+                break;
+                }
             break;
         }
-        board.addPiece(piece, location[][]);
-    }else{
-        throw exception("Invalid Move")
+        board.addPiece(type, location);
     }
+
+    // method to move the piece
+    void movePiece(String direction, int steps){
     
-}
+        if(isValidMove(direction, steps)){
+            board.removePiece(location);
+            //update location
+            if(strcmp(direction, "NE") == 0){
+                for(int i = 0; i < 3; i++){//up
+                    location[i][0] = location[i][0] + steps;
+                }
+            } else if(strcmp(direction, "NW")==0){//left
+                for(int i = 0; i < 3; i++){
+                    location[i][1] = location[i][1] - steps;
+                }
+            } else if(strcmp(direction, "SE")==0){//right
+                for(int i = 0; i < 3; i++){
+                    location[i][1] = location[i][1] + steps;
+                }
+            } else if(strcmp(direction, "SW")==0){//down
+                for(int i = 0; i < 3; i++){
+                    location[i][0] = location[i][0] - steps;
+                }
+            }
+
+            board.addPiece(type, location);
+            if(type == 0){
+                if(checkVictoryCondition()){
+                //throw exception("Victory");
+                }
+            }
+        } else{
+            //throw exception("Invalid Move");
+        }
+    }
+
+
+    // method to check victory condition
+    bool checkVictoryCondition(){
+        //check if red virus is in the appropriate location
+        if ((location[0][0] == 0 && location[0][1] == 5) || (location[1][0] == 0 && location[1][1] == 5)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool isValidMove(string direction, int steps){
+    }
+
+};
