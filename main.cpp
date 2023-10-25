59a0bf04de82f56e77c0232259b05c37d89da0c9
Boden Retherford <57460166+BodenRetherford@users.noreply.github.com>
2023-10-25 12:38:10 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,3 +5,17 @@
 #include "piece.hpp"
 
 using namespace std;
+
+int main{
+    board gameboard;
+    piece[] pieces;
+
+    int[] eek = [3,4]
+    piece virus('0',eek ,0);
+
+    gameboard.Frontend();
+    
+    //virus.move()
+
+    return 0;
+}
